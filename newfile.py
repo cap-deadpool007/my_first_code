b30c517458c5a598a29977366ed9877d74e77224
cap-deadpool007 <43106971+cap-deadpool007@users.noreply.github.com>
2019-01-18 11:05:39 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#import is a keyword which import library in python
+import time
+import math
+
+x=raw_input("please enter your birth date's first digit:      ")
+y=raw_input("please enter your birth date's second digit:      ")
+
+print  "processing..................()()()()()"
+#   here system is a function under os library  to run any command 
+
+time.sleep(4) #here 4 is second
+
+
+sum = int(x)+int(y)
+
+print    'your lucky no is' 
+print     sum
